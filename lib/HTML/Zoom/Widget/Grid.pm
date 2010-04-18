a9c699d5dc26ec6677609c5afa43d235c9afdb1c
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-04-17 21:03:29 -0500

    initial commit

---
+++ lib/HTML/Zoom/Widget/Grid.pm
@@ -0,0 +1,119 @@
+package HTML::Zoom::Widget::Grid;
+
+use 5.12.0;
+
+use HTML::Zoom;
+use Method::Signatures::Simple;
+use Moose;
+use List::MoreUtils 'natatime';
+
+has config => (
+   is => 'ro',
+   isa => 'HashRef',
+   lazy_build => 1,
+);
+
+method column_spec {
+   use HTML::Tags;
+   my $col_id;
+   my @inner = (<tr>);
+   for (@{$self->config->{columns}}) {
+      $col_id++;
+      my $col_class = (defined $_->{column_class}?"$_->{column_class} ":'');
+      push @inner, <td class="${col_class}col-$col_id">,</td>;
+   }
+   push @inner, </tr>;
+
+   join '', HTML::Tags::to_html_string(@inner)
+}
+
+method render_row_data($data) {
+   my @rows;
+   for my $data_item (@{$data}) {
+      my @columns;
+
+      my $col_id = 1;
+      for my $conf (@{$self->config->{columns}}) {
+         my $value    = $data_item->{$conf->{data_index}};
+         my $renderer = $conf->{renderer} || sub { $_[1] };
+         push @columns, $self->$renderer($value, $data_item);
+
+         $col_id++;
+      }
+      push @rows, \@columns;
+   }
+   return \@rows;
+}
+
+method render_rows($rendered_data) {
+   [map {
+      my $val = $_;
+      sub {
+         my $r = $_;
+         my $x = 1;
+         $r = $r->select(".col-$x")->replace_content($_)
+            for @{$val};
+         $r
+      }
+   } @{$rendered_data}];
+}
+
+method insert_into_html($tpl, $data) {
+   my $col_spec = $self->column_spec;
+
+   HTML::Zoom
+      ->from_html($tpl)
+      ->select($self->config->{render_to})
+      ->replace_content(\'<table></table>')
+      ->select('table')
+      ->replace_content(\$col_spec)
+      ->then
+      ->repeat_content($self->render_rows($self->render_row_data($data)))
+      ->to_html
+}
+
+1;
+__END__
+
+Sketch:
+
+use MooseX::Declare;
+class UserGrid extends HTML::Zoom::Widget::Grid {
+
+  method id_column {
+    return id => {
+      # no renderer used so value is printed directly
+      value_field => 'id',
+    }
+  }
+
+  mehod email_column {
+    return email => {
+      renderer => 'email_renderer',
+      value_field => 'email',
+    }
+  }
+
+  method name_column {
+    return name => {
+      renderer => 'name_renderer',
+      # no value field is used since it's effectively a composite field
+    }
+  }
+
+  method columns {
+    return {
+      $self->id_column,
+      $self->email_column,
+      $self->name_column,
+    }
+  }
+
+  method name_renderer($value, $row) {
+    "$row->{firstname} $row->{lastname}"
+  }
+
+  method email_renderer($value, $row) {
+    "<a href='email:$value'>$value</a>"
+   }
+};
---
+++ t/initial_play.t
@@ -0,0 +1,45 @@
+#!perl
+
+use 5.11.0;
+
+use warnings;
+
+use HTML::Zoom;
+use aliased 'HTML::Zoom::Widget::Grid';
+
+  my $template = <<HTML;
+<html>
+  <head>
+    <title>Hello people</title>
+  </head>
+  <body>
+    <div id="grid1"></div>
+  </body>
+</html>
+HTML
+
+my $grid = Grid->new({ config => {
+   render_to => 'div#grid1',
+   columns => [
+      {
+         data_index => 'id',
+         renderer => sub { $_[1] * 2 },
+      }, {
+         renderer => sub { $_[1] . ' ' . $_[2]->{id} },
+         data_index => 'name',
+         column_class => 'frewbot',
+      },
+   ]
+}});
+
+print $grid->insert_into_html($template, {
+   data => [{
+      id => 1,
+      name => 'frew',
+   }, {
+      id => 2,
+      name => 'wes',
+   }],
+   total => 100,
+);
+
